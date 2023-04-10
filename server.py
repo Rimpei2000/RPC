144ce003d21c22b7f4f4cb7f55081c56ae849dc8
Rimpei <70897624+Rimpei2000@users.noreply.github.com>
2023-04-11 03:53:31 +0900

    initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+# create a socket
+socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# bind the socket to a server address
+socket.bind()
+
+# open the socket and listen for a request
+socket.listen()
+
+def floor(num):
+    pass
+
+def nroot():
+    pass
+
+def reverse(input_str):
+    return input_str[::-1]
+
+def validAnagram(input_str_1, input_str_2):
+    pass
+
+def sort(str_arr):
+    pass
