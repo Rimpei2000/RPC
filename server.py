4250d37a4e6b0704cd5ef7fc11b4c9f268b42eca
Rimpei <70897624+Rimpei2000@users.noreply.github.com>
2023-04-11 23:27:52 +0900

    completed server

--- server.py
+++ server.py
@@ -25,6 +25,56 @@ class Server:
             client_thread = threading.Thread(target=self.accepted, args=(connection, ))
             client_thread.start()
 
+    def accepted(self, connection):
+        try:
+            while True:
+                recv_data = connection.recv(1024).decode('utf-8')
+                print('Received data: {}'.format(recv_data))
+                print('-----------------------------------')
+                if recv_data:
+                    self.respond(connection, recv_data)
+                else:
+                    print('No data')
+                    break
+        finally:
+            print('Closing current connection')
+            connection.close()
+    
+    def respond(self, connection, recv_data):
+        try:
+            response = JsonProcessor.process(json.loads(recv_data))
+            connection.sendadd(response.encode())
+        except Exception as e:
+            print('Error occurred: {}'.format(str(e)))
+            error_str = { "error": str(e) }
+            connection.sendall(json.dumps(error_str).encode())
+            
+class JsonProcessor:
+    processor = {
+        "floor": (lambda x: math.floor(x), "int"),
+        "nroot": (lambda params: (params[1] ** (1/params[0])), "double"),
+        "reverse": (lambda s: s[::-1], "string"),
+        "validAnagram": (lambda params: set(params[0]) == set(params[1]), "string"),
+        "sort": (lambda strArr: sorted(strArr), "string[]")
+    }
+
+    def process(jsondata):
+        try:
+            method = jsondata["method"]
+            params = jsondata["params"]
+            param_types = jsondata["param_types"]
+            id = jsondata["id"]
+
+            res_method = JsonProcessor.processor[method][0](params)
+            res_type = JsonProcessor.processor[method][1]
+            response = { "result": res_method, "result_types": res_type, "id": id }
+            return json.dumps(response)
+        
+        except Exception as e:
+            print('Error occurred: {}'.format(e))
+            response = { "error": str(e) }
+            return json.dumps(response)
+
 def main():
     socket_path = './socket_path'
     server = Server(socket_path)
