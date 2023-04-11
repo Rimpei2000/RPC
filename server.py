f2b648f4d974ecc7ddee66883d0f6b48cabb0463
Rimpei <70897624+Rimpei2000@users.noreply.github.com>
2023-04-11 14:06:49 +0900

    implemented server starting function

--- server.py
+++ server.py
@@ -1,52 +1,34 @@
 import socket
 import os
+import json
+import math
+import threading
 
-# create a socket
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+class Server:
+    def __init__(self, socket_path):
+        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._socket_path = socket_path
+    
+    def start(self):
+        try:
+            os.unlink(self._socket_path)
+        except FileNotFoundError:
+            pass
+    
+        print('Starting up on {}'.format(self._socket_path))
+        self._socket.bind(self._socket_path)
+        self._socket.listen(1)
+        
+        while True:
+            connection, client_address = self._socket.accept()
+            print('Accepted connection')
+            client_thread = threading.Thread(target=self.accepted, args=(connection, ))
+            client_thread.start()
 
-server_address = './socket_file'
+def main():
+    socket_path = './socket_path'
+    server = Server(socket_path)
+    server.start()
 
-try:
-    os.unlink(server_address)
-except FileNotFoundError:
-    pass
-
-# bind the socket to a server address
-print('Starting up on {}'.format(server_address))
-sock.bind(server_address)
-
-# open the socket and listen for a request
-sock.listen(1)
-
-def floor(num):
-    pass
-
-def nroot(n, x):
-    pass
-
-def reverse(input_str):
-    return input_str[::-1]
-
-def validAnagram(input_str_1, input_str_2):
-    pass
-
-def sort(str_arr):
-    pass
-
-while True:
-    connection, client_address = sock.accept()
-
-    try:
-        data = connection.recv(16)
-        data_str = data.decode('utf-8')
-        print('Received ' + data_str)
-
-        if data:
-            response = 'Processing ' + data_str
-            # connection.sendall(response.encode())
-        else:
-            print('no data from', client_address)
-            break
-    finally:
-        print('Closing current connection')
-        connection.close()
+if __name__ == "__main__":
+    main()
